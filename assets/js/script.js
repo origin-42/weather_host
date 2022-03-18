3b59e731498e9cecf44170fe3738a1297baa3d6d
Origin 42 <daveplum1991@gmail.com>
2022-03-19 02:17:18 +0800

    created object to capture user city input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,79 @@ let city = 'Western Australia';
 let queryURLWeather = `http://api.openweathermap.org/data/2.5/weather`;
 let queryURLForcast = `http://api.openweathermap.org/data/2.5/forecast`;
 
+const searchHistory = {};
+
 // Update city and call query with fetch
-fetch(`${queryURLForcast}?q=${city}&appid=${apiKey}`)
-.then(function (response) {
+const queryWeather = (givenCity) => {
 
-    console.log(response)
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-})
+    // Collect data
+    let weatherData;
+    let forecastData;
 
-fetch(`${queryURLWeather}?q=${city}&appid=${apiKey}`)
-.then(function (response) {
+    fetch(`${queryURLForcast}?q=${city}&appid=${apiKey}`)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error (err);
+        }
+        
+    })
+    .then(function (weatherSuccessData) {
+        weatherData = weatherSuccessData;
+    })
+    .catch((err) => {
+        // Add feedback for user;
+        // Possible city incorrectly entered or not a city
+    });
 
-    console.log(response)
-    return response.json();
-})
-.then(function (data) {
-    console.log(data);
-})
+    fetch(`${queryURLWeather}?q=${city}&appid=${apiKey}`)
+    .then(function (response) {
+
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error (err);
+        }
+    })
+    .then(function (forecastSuccessData) {
+        forecastData = forecastSuccessData;
+    })
+    .catch((err) => {
+        // Add feedback for user;
+        // Possible city incorrectly entered or not a city
+    });
+
+    // Call event handler success function. Which will;
+    saveData(givenCity, weatherData, forecastData);
+    // take the data, call another function to handle adding that search into a history object
+    // Call function to extract from that object and display it's info in correct formatting.
+}
+
+
+
+
+// Sanitise user entry
+const sanitiseEntry = (event) => {
+    event.preventDefault();
+
+    let value = $("#citysearch").val()
+    let restrictions = /^[-a-zA-Z]+(\s+[-a-zA-Z]+)*$/;
+
+    if (value.match(restrictions)) {
+        // Gives sanitised value;
+        city = value;
+    } else {
+        // Add feedback for user;
+    }
+
+    addSearchHistory(value);
+    
+} 
+
+const addSearchHistory = (city) => {
+
+}
+
+// Event Listeners
+$("#citysearch_submit").on('click', sanitiseEntry);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Weather Host
+
+# credits
+Regular expression, remove appending and prepending whitespace. Letters only. Open weather API accepts both cases.
+https://stackoverflow.com/questions/34974942/regex-for-no-whitespace-at-the-beginning-and-end
