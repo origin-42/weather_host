4b3478ffdf33a4cc4e61663d7c63d0855517d701
Origin 42 <daveplum1991@gmail.com>
2022-03-19 14:41:22 +0800

    adjusted styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 let apiKey = 'd3ddf23d533942a91d17b7f565e673f9';
-let city = 'Western Australia';
+let city;
 let queryURLWeather = `http://api.openweathermap.org/data/2.5/weather`;
 let queryURLForcast = `http://api.openweathermap.org/data/2.5/forecast`;
 
 const searchHistory = {};
 
 // Update city and call query with fetch
-const queryWeather = (givenCity) => {
+const queryWeather = (citName) => {
 
     // Collect data
     let weatherData;
@@ -19,40 +19,41 @@ const queryWeather = (givenCity) => {
         } else {
             throw new Error (err);
         }
-        
     })
     .then(function (weatherSuccessData) {
         weatherData = weatherSuccessData;
+    }).then(function() {
+
+        fetch(`${queryURLWeather}?q=${city}&appid=${apiKey}`)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error (err);
+            }
+        })
+        .then(function (forecastSuccessData) {
+            forecastData = forecastSuccessData;
+        })
+        .catch((err) => {
+            // Add feedback for user;
+            // Possible city incorrectly entered or not a city
+        }).then( function() {
+            addSearchHistory(citName, weatherData, forecastData);
+        });
     })
     .catch((err) => {
         // Add feedback for user;
         // Possible city incorrectly entered or not a city
     });
 
-    fetch(`${queryURLWeather}?q=${city}&appid=${apiKey}`)
-    .then(function (response) {
-
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error (err);
-        }
-    })
-    .then(function (forecastSuccessData) {
-        forecastData = forecastSuccessData;
-    })
-    .catch((err) => {
-        // Add feedback for user;
-        // Possible city incorrectly entered or not a city
-    });
+    
 
     // Call event handler success function. Which will;
-    saveData(givenCity, weatherData, forecastData);
+    
     // take the data, call another function to handle adding that search into a history object
     // Call function to extract from that object and display it's info in correct formatting.
 }
-
-
 
 
 // Sanitise user entry
@@ -69,11 +70,32 @@ const sanitiseEntry = (event) => {
         // Add feedback for user;
     }
 
-    addSearchHistory(value);
+    queryWeather(value);
     
 } 
 
-const addSearchHistory = (city) => {
+const addSearchHistory = (city, weather, forecast) => {
+
+    // <!-- Displayed information for weather -->
+        // <!-- City name, date, and weather icon -->
+        // <!-- Temp -->
+        // <!-- Wind -->
+        // <!-- Humidity -->
+        // <!-- UV Index -->
+    
+    // <!-- 5 day forecase -->
+        // <!-- Day 1 -->
+            // <!-- Date -->
+            // <!-- Weather Rep Icon -->
+            // <!-- Temp -->
+            // <!-- Wind -->
+            // <!-- Humidity -->
+        // <!-- Day 2 -->
+        // <!-- Day 3 -->
+        // <!-- Day 4 -->
+        // <!-- Day 5 -->
+        
+    searchHistory["cities"] = {"city": city, "weather": weather, "forecast": forecast,}
 
 }
 
